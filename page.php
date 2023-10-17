1ac4be03404e840e9b9529a78f6115bdbf1ecf10
AurelienAugem <130144174+AurelienAugem@users.noreply.github.com>
2023-10-17 15:05:13 +0200

    formulaire contact phase 2

--- page.php
+++ page.php
@@ -1,5 +1,14 @@
 <?php get_header()?>
 <main>
-    
+<?php    
+    if (have_posts()):
+        while (have_posts()) : the_post();
+            the_content();
+        endwhile;
+    endif;
+?>
+<?php if(is_front_page()): ?>
+
+<?php endif;?>    
 </main>
 <?php get_footer()?>
